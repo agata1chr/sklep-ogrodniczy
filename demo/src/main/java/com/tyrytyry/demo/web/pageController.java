38d0e37cc07f9f150219e6e3d339cc6fb02906e0
Tyrytyry <106188035+Tyrytyry@users.noreply.github.com>
2023-04-16 16:25:12 +0200

    Add files via upload

---
+++ demo/src/main/java/com/tyrytyry/demo/web/pageController.java
@@ -0,0 +1,27 @@
+package com.tyrytyry.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class pageController {
+
+
+    @GetMapping("/")
+    public String orderForm() {
+
+        return "home";
+
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+
+        return "home";
+    }
+
+
+}
---
+++ demo/src/main/java/com/tyrytyry/demo/web/pagecategory.java
@@ -0,0 +1,19 @@
+package com.tyrytyry.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class pagecategory {
+
+
+        @GetMapping("/category")
+        public String pageCategory() {
+
+
+            return "category.html";
+        }
+}
+
+
+
