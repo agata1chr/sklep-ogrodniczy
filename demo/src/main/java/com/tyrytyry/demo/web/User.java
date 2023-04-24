492d0357875226a4c6fff7fef3eb9383805ab13d
Mateusz Jarosz <mateuszjarosz55@interia.pl>
2023-04-24 21:49:29 +0200

    good version

---
+++ demo/src/main/java/com/tyrytyry/demo/web/User.java
@@ -0,0 +1,24 @@
+package com.tyrytyry.demo.web;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NonNull
+    private String username;
+    @NonNull
+    private String password;
+
+}
---
+++ demo/src/main/java/com/tyrytyry/demo/web/UserRepository.java
@@ -0,0 +1,8 @@
+package com.tyrytyry.demo.web;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>  {
+}
